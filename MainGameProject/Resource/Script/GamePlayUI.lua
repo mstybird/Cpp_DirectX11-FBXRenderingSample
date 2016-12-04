05012e9cf6a64578539d096c48e8638124a2ef55
mstybird <0235reimu@gmail.com>
2016-12-05 00:03:32 +0900

    UIリソースの更新

--- MainGameProject/Resource/Script/GamePlayUI.lua
+++ MainGameProject/Resource/Script/GamePlayUI.lua
@@ -7,14 +7,20 @@ StatusFrameTexturePath="Resource/UI/Playing/StatusFrame.png"
 --HPBar
 HPBarPosition={20,40}
 HPBarSize={140,20}
-HPBarInScale={0.9,0.9}
+HPBarInScale={0.95,0.9}
 HPBarOutTexturePath="Resource/UI/Playing/HPBarOut.png"
 HPBarInTexturePath="Resource/UI/Playing/HPBarIn.png"
 --EPBar
 EPBarPosition={20,80}
 EPBarSize={140,20}
-EPBarInScale={0.9,0.9}
+EPBarInScale={0.95,0.9}
 EPBarOutTexturePath="Resource/UI/Playing/EPBarOut.png"
 EPBarInTexturePath="Resource/UI/Playing/EPBarIn.png"
 
-
+--ScoreBar
+ScoreBarPosition={20,80}
+ScoreBarSize={140,20}
+ScoreBarInScale={0.9,0.9}
+ScoreBarOutTexturePath="Resource/UI/Playing/ScoreBarOut.png"
+ScoreBarInTexturePath="Resource/UI/Playing/ScoreBarLeft.png"
+ScoreBarInTexturePath="Resource/UI/Playing/ScoreBarRight.png"
--- MainGameProject/Resource/Script/TitleUI.lua
+++ MainGameProject/Resource/Script/TitleUI.lua
@@ -12,17 +12,19 @@ TitleScale={1.0,1.0}
 TitleTexturePath="Resource/UI/Title/Title.png"
 
 --ButtonStart
-ButtonPosition={100,200}
-ButtonSize={160,30}
-ButtonScale={1.0,1.0}
-ButtonNormalTexturePath="Resource/UI/Title/ButtonStart_Normal.png"
-ButtonPushTexturePath="Resource/UI/Title/ButtonStart_Push.png"
-ButtonNoActiveTexturePath="Resource/UI/Title/ButtonStart_NoActive.png"
+ButtonStartPosition={100,200}
+ButtonStartSize={160,30}
+ButtonStartScale={1.0,1.0}
+ButtonStartNormalTexturePath="Resource/UI/Title/ButtonStart_Normal.png"
+ButtonStartPushTexturePath="Resource/UI/Title/ButtonStart_Push.png"
+ButtonStartNoActiveTexturePath="Resource/UI/Title/ButtonStart_NoActive.png"
+ButtonStartDisableTexturePath="Resource/UI/Title/ButtonStart_NoActive.png"
 
---ButtonExit
-ButtonPosition={100,200}
-ButtonSize={160,30}
-ButtonScale={1.0,1.0}
-ButtonNormalTexturePath="Resource/UI/Title/ButtonStart_Normal.png"
-ButtonPushTexturePath="Resource/UI/Title/ButtonStart_Push.png"
-ButtonNoActiveTexturePath="Resource/UI/Title/ButtonStart_NoActive.png"
+--ButtonExitExit
+ButtonExitPosition={100,200}
+ButtonExitSize={160,30}
+ButtonExitScale={1.0,1.0}
+ButtonExitNormalTexturePath="Resource/UI/Title/ButtonExit_Normal.png"
+ButtonExitPushTexturePath="Resource/UI/Title/ButtonExit_Push.png"
+ButtonExitNoActiveTexturePath="Resource/UI/Title/ButtonExit_NoActive.png"
+ButtonExitDisableTexturePath="Resource/UI/Title/ButtonExit_NoActive.png"
